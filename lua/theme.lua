f43a38e570eadfce977e5adacb8675f15b136eb6
Jon Eskin <eskinjp@gmail.com>
2022-07-29 02:52:04 -0400

    removing old code

--- lua/theme.lua
+++ lua/theme.lua
@@ -1,6 +1,8 @@
 local colors = require('colors')
-
 local theme = {}
+
+-- I'd like to revamp the architecture of coloring and add configurable themes
+-- The existing design is entirely placeholder and is expected to be overhauled
 
 local default_theme = {
 	-- bold => bright
@@ -27,32 +29,6 @@ local default_theme = {
 	['embedded'] = colors.magenta .. colors.bright,
 	['identifier'] = colors.white,
 }
-
--- local highlight_theme = {
-	-- -- bold => bright
-	-- -- italics => underscore
-	-- ['default'] =  colors.white .. colors.reverse,
-	-- ['nothing'] = '' .. colors.reverse,
-	-- ['class'] = colors.yellow .. colors.bright .. colors.reverse,
-	-- ['comment'] = colors.blue .. colors.bright .. colors.reverse,
-	-- ['constant'] = colors.cyan .. colors.bright .. colors.reverse,
-	-- ['definition'] = colors.blue .. colors.bright .. colors.reverse,
-	-- ['error'] = colors.red .. colors.underscore .. colors.reverse,
-	-- ['function'] = colors.blue .. colors.bright .. colors.reverse,
-	-- ['keyword'] = colors.yellow .. colors.bright .. colors.reverse,
-	-- ['label'] = colors.green .. colors.bright .. colors.reverse,
-	-- ['number'] = colors.red .. colors.bright .. colors.reverse,
-	-- ['operator'] = colors.cyan .. colors.bright .. colors.reverse,
-	-- ['regex'] = colors.green .. colors.bright .. colors.reverse,
-	-- ['string'] = colors.red .. colors.bright .. colors.reverse,
-	-- ['preprocessor'] = colors.magenta .. colors.bright .. colors.reverse,
-	-- ['tag'] = colors.red .. colors.bright .. colors.reverse,
-	-- ['type'] = colors.green .. colors.bright .. colors.reverse,
-	-- ['variable'] = colors.blue .. colors.bright .. colors.reverse,
-	-- ['whitespace'] =  '' .. colors.reverse,
-	-- ['embedded'] = colors.onblue .. colors.bright .. colors.reverse,
-	-- ['identifier'] = colors.white .. colors.reverse,
--- }
 
 local highlight_theme = {
 	-- bold => bright
