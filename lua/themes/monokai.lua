185c937e17d8d57767e122f732199ee5a6bdaacc
Jon Eskin <eskinjp@gmail.com>
2022-09-11 14:28:46 -0400

    add solarized and monokai themes

---
+++ lua/themes/monokai.lua
@@ -0,0 +1,24 @@
+local M = {}
+M.theme = {
+	['default'] = "#f8f8f2",
+	['nothing'] = '',
+	['class'] = "#f92672",
+	['comment'] = "#75715e",
+	['constant'] = "#f8f8f2",
+	['error'] = "#f8f8f0",
+	['function'] = "#a6e22e",
+	['keyword'] = "#f92672",
+	['label'] = "#e6db74",
+	['number'] = "#ae81ff",
+	['operator'] = "#f8f8f2",
+	['regex'] = "#ae81ff",
+	['string'] = "#e6db74",
+	['preprocessor'] = "#f92672",
+	['tag'] = "#f92672",
+	['type'] = "#66d9ef",
+	['variable'] = "#66d9ef",
+	['whitespace'] = '',
+	['embedded'] = "#e6db74",
+	['identifier'] = "#f8f8f2",
+}
+return M
